03d9c556f29a8a240018ee11f9a9703a6ad10cec
kiran3824 <149896912+kiran3824@users.noreply.github.com>
2024-02-08 19:40:08 +0530

    Add files via upload

---
+++ Day 49_ First Missing Positive.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int missingNumber = 1;
+        Arrays.sort(nums);
+        for (int number : nums) {
+            if (number > 0) {
+                if (number == missingNumber)
+                    missingNumber++;
+                else if (number > missingNumber)
+                    break;
+            }
+        }
+        return missingNumber;
+    }
+}
